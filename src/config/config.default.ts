562057777a4c2d698a72eeb3240706fb78dfcec4
dangdang-01 <3180422969@qq.com>
2022-07-28 16:05:25 +0800

    first commit

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1658994051208_1031',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
